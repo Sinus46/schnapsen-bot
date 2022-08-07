f97ba47e0a8baec161ad2f79c14ced13d593f58b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-07 23:48:23 +0300

    AI System

--- src/Sinus46/App/App.java
+++ src/Sinus46/App/App.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class App extends JFrame implements ActionListener {
+    public static final String NORMAL_AI = "Normal";
+    public static final String UNFAIR_AI = "Unfair";
+    public static final String GODLY_AI = "Godly";
     private static App instance = null;
 
     public static void init(){
@@ -27,17 +30,22 @@ public class App extends JFrame implements ActionListener {
     private Container mainMenu() {
         JPanel pane = new JPanel();
 
-        JButton normal = new JButton("Normal");
+        JButton normal = new JButton(NORMAL_AI);
         normal.setMaximumSize(new Dimension(100, 30));
-        JButton hard = new JButton("Hard");
+        JButton hard = new JButton(UNFAIR_AI);
         hard.setMaximumSize(new Dimension(100, 30));
+        JButton godly = new JButton(GODLY_AI);
+        godly.setMaximumSize(new Dimension(100, 30));
         normal.addActionListener(this);
         hard.addActionListener(this);
+        godly.addActionListener(this);
 
         Box box = new Box(BoxLayout.Y_AXIS);
         box.add(normal);
         box.add(Box.createVerticalStrut(5));
         box.add(hard);
+        box.add(Box.createVerticalStrut(5));
+        box.add(godly);
 
         pane.add(box);
         SpringLayout manager = new SpringLayout();
